dda16acae80378b8a583279fc57aeca39e2db486
root <root@DESKTOP-GH72CDF.localdomain>
2026-04-25 07:45:25 -0500

    fix: menu public-3

--- frontend/src/app/shared/components/public-navbar/public-navbar.ts
+++ frontend/src/app/shared/components/public-navbar/public-navbar.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+export class NavbarComponent {
+  isMenuOpen = false; // Controla si el menú se ve o no
+}
 
 @Component({
   selector: 'app-public-navbar',
